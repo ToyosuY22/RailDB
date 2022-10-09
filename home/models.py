a8e7a88118616ab8571297d6e858e419a7bcca6d
Toyosu Y-22 <doing.zuya@gmail.com>
2022-10-09 22:54:22 +0900

    add: createsuperuser が動くように改良

--- home/models.py
+++ home/models.py
@@ -3,10 +3,43 @@ import uuid
 
 from django.conf import settings
 from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import PermissionsMixin, UserManager
 from django.db import models
 from django.urls import reverse
 from django.utils import timezone
+
+
+class RailDBUserManager(UserManager):
+    """username field が存在しないケースに対応
+
+    https://github.com/django/django/blob/f0c06f8ab7904e1fd082f2de57337f6c7e05f177/django/contrib/auth/models.py#L136
+    """
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Create and save a user with the given username, email, and password.
+        """
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.password = make_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email=None, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -58,7 +91,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         help_text='無効にするとログインできなくなります',
     )
 
-    objects = UserManager()
+    objects = RailDBUserManager()
 
 
 class EmailToken(models.Model):
